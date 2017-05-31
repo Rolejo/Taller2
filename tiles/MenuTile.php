1b930c31f996eb307318e4edc1eb012d5d5c4eec
Rolejo <rolejo.rock@hotmail.com>
2017-05-31 19:24:26 -0400

    Add files via upload

---
+++ tiles/MenuTile.php
@@ -0,0 +1,24 @@
+<div class="nav-wrapper yellow-text blue darken-4">
+    <a href="#" class="right brand-logo">Logo</a>
+    <ul id="nav-mobile" class="center hide-on-med-and-down">
+        <li><a href="inicio.php">Inicio</a></li>
+        <li><a href="somos.php">¿Qui&eacute;nes somos?</a></li>
+        <li><a href="eventos.php">Eventos</a></li>
+        <li><a href="contacto.php">Contacto</a></li>
+        <li><a href="login.php">* Ingresar *</a></li>
+    </ul>
+
+    <ul id="nav-mobile01" class="side-nav" style="transform: translateX(-100%);">
+        <li><a href="inicio.php">Inicio</a></li>
+        <li><a href="somos.php">¿Qui&eacute;nes somos?</a></li>
+        <li><a href="eventos.php">Eventos</a></li>
+        <li><a href="contacto.php">Contacto</a></li>
+        <li><a href="login.php">* Ingresar *</a></li>
+    </ul>
+
+    <a href="#" data-activates="nav-mobile01" class="button-collapse"><i class="material-icons">menu</i></a>
+</div>
+
+<script type="text/javascript">
+    $('.button-collapse').sideNav();
+</script>
